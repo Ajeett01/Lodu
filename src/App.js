29b4da5ebef8cc67dc0d1a331cf2582abb26513e
Ajeett01 <abhitiwari501@gmail.com>
2023-09-04 00:38:26 +0530

    AddedCard

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import BasicModal from './EditTaskModal';
 import BasicButtons from './Button';
 import { CheckBox } from '@mui/icons-material';
 import Checkboxes from './checkbox';
+import RecipeReviewCard from './Card';
 
 const App = () => {
   return (
@@ -14,6 +15,7 @@ const App = () => {
       <TemporaryDrawer />
       <BasicCard/>
       <BasicModal/>
+      <RecipeReviewCard/>
     </div>
   );
 }
